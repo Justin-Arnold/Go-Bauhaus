f245a695691e83361c51c1e01738f7b1359a5a60
Justin Arnold <justin.arnold@programmer.net>
2023-10-20 14:11:50 -0400

    add ability to get random quote

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
 )
+type Quote struct {
+	Content string `json:"content"`
+	Author  string `json:"author"`
+}
 
 func main() {
 	http.Handle("/static/",
@@ -18,6 +24,22 @@ func main() {
 	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
 		tmpl := template.Must(template.ParseFiles("./templates/fragments/surprise.html"))
 		tmpl.Execute(w, nil)
+	})
+
+	http.HandleFunc("/quote", func(w http.ResponseWriter, r *http.Request) {
+		tmpl := template.Must(template.ParseFiles("./templates/fragments/quote.html"))
+		resp, err := http.Get("https://api.quotable.io/random")
+		if err != nil {
+			log.Fatal(err)
+		}
+		body, err := io.ReadAll(resp.Body)
+
+		data := struct {
+			Content string `json:"content"`
+			Author  string `json:"author"`
+		}{}
+		json.Unmarshal(body, &data)
+		tmpl.Execute(w, data)
 	})
 
 	log.Println("Bauhaus running on localhost:8000")
